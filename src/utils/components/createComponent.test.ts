2427538f6c7f4f19c0afe862a6ae0d331759374d
Grigoris Christainas <grigorisxristainas@gmail.com>
2024-03-23 18:34:29 +0200

    Added tests for createComponent util

---
+++ src/utils/components/createComponent.test.ts
@@ -0,0 +1,263 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import {
+    mockComponentTemplateBasic,
+    mockComponentTemplateFull,
+    mockComponentTemplateNoStyles,
+    mockComponentTemplateNoTypes,
+    mockIndexTemplate,
+    mockStylesTemplate,
+    mockTypesTemplate,
+} from '../../templates/mocks'
+import createComponent from './createComponent'
+import chalk from 'chalk'
+import {
+    componentName,
+    sandboxDirName,
+    targetDirectory,
+    fileContainsContent,
+    componentFilePath,
+    indexFilePath,
+    stylesFilePath,
+    typesPath,
+} from './mocks'
+
+const consoleLogMock = jest.spyOn(console, 'log').mockImplementation()
+
+describe('createComponent: Test that util', () => {
+    describe('With default extra arguments', () => {
+        beforeAll(() => {
+            fs.rmSync(targetDirectory, { recursive: true, force: true })
+        })
+
+        beforeEach(() => {
+            consoleLogMock.mockClear()
+        })
+
+        afterAll(() => {
+            fs.rmSync(targetDirectory, { recursive: true, force: true })
+        })
+
+        it('should log successful messages', async () => {
+            await createComponent(componentName, sandboxDirName, false, false)
+
+            expect(consoleLogMock).toHaveBeenCalledTimes(2)
+
+            expect(consoleLogMock).toHaveBeenCalledWith(
+                chalk.green('\nComponent has been successfully created!')
+            )
+
+            expect(consoleLogMock).toHaveBeenCalledWith(
+                chalk.cyan('\nHappy coding 🎉')
+            )
+        })
+
+        it('should create files correctly with correct content', () => {
+            expect(
+                fileContainsContent(indexFilePath, mockIndexTemplate)
+            ).toEqual(true)
+
+            expect(
+                fileContainsContent(
+                    componentFilePath,
+                    mockComponentTemplateFull
+                )
+            ).toEqual(true)
+
+            expect(
+                fileContainsContent(stylesFilePath, mockStylesTemplate)
+            ).toEqual(true)
+
+            expect(fileContainsContent(typesPath, mockTypesTemplate)).toEqual(
+                true
+            )
+        })
+
+        it('should log error message when trying to create component in existing directory', async () => {
+            await createComponent(componentName, sandboxDirName, false, false)
+
+            expect(consoleLogMock).toHaveBeenCalledTimes(1)
+
+            expect(consoleLogMock).toHaveBeenCalledWith(
+                chalk.red(
+                    '\nLooks like component already exists, please try deleting its directory and try again.'
+                )
+            )
+        })
+    })
+
+    describe('With styles excluded', () => {
+        beforeAll(() => {
+            fs.rmSync(targetDirectory, { recursive: true, force: true })
+        })
+
+        beforeEach(() => {
+            consoleLogMock.mockClear()
+        })
+
+        afterAll(() => {
+            fs.rmSync(targetDirectory, { recursive: true, force: true })
+        })
+
+        it('should log successful messages', async () => {
+            await createComponent(componentName, sandboxDirName, true, false)
+
+            expect(consoleLogMock).toHaveBeenCalledTimes(2)
+
+            expect(consoleLogMock).toHaveBeenCalledWith(
+                chalk.green('\nComponent has been successfully created!')
+            )
+
+            expect(consoleLogMock).toHaveBeenCalledWith(
+                chalk.cyan('\nHappy coding 🎉')
+            )
+        })
+
+        it('should create files correctly with correct content', () => {
+            expect(
+                fileContainsContent(indexFilePath, mockIndexTemplate)
+            ).toEqual(true)
+
+            expect(
+                fileContainsContent(
+                    componentFilePath,
+                    mockComponentTemplateNoStyles
+                )
+            ).toEqual(true)
+
+            expect(fs.existsSync(stylesFilePath)).toEqual(false)
+
+            expect(fileContainsContent(typesPath, mockTypesTemplate)).toEqual(
+                true
+            )
+        })
+
+        it('should log error message when trying to create component in existing directory', async () => {
+            await createComponent(componentName, sandboxDirName, false, false)
+
+            expect(consoleLogMock).toHaveBeenCalledTimes(1)
+
+            expect(consoleLogMock).toHaveBeenCalledWith(
+                chalk.red(
+                    '\nLooks like component already exists, please try deleting its directory and try again.'
+                )
+            )
+        })
+    })
+
+    describe('With types excluded', () => {
+        beforeAll(() => {
+            fs.rmSync(targetDirectory, { recursive: true, force: true })
+        })
+
+        beforeEach(() => {
+            consoleLogMock.mockClear()
+        })
+
+        afterAll(() => {
+            fs.rmSync(targetDirectory, { recursive: true, force: true })
+        })
+
+        it('should log successful messages', async () => {
+            await createComponent(componentName, sandboxDirName, false, true)
+
+            expect(consoleLogMock).toHaveBeenCalledTimes(2)
+
+            expect(consoleLogMock).toHaveBeenCalledWith(
+                chalk.green('\nComponent has been successfully created!')
+            )
+
+            expect(consoleLogMock).toHaveBeenCalledWith(
+                chalk.cyan('\nHappy coding 🎉')
+            )
+        })
+
+        it('should create files correctly with correct content', () => {
+            expect(
+                fileContainsContent(indexFilePath, mockIndexTemplate)
+            ).toEqual(true)
+
+            expect(
+                fileContainsContent(
+                    componentFilePath,
+                    mockComponentTemplateNoTypes
+                )
+            ).toEqual(true)
+
+            expect(
+                fileContainsContent(stylesFilePath, mockStylesTemplate)
+            ).toEqual(true)
+
+            expect(fs.existsSync(typesPath)).toEqual(false)
+        })
+
+        it('should log error message when trying to create component in existing directory', async () => {
+            await createComponent(componentName, sandboxDirName, false, false)
+
+            expect(consoleLogMock).toHaveBeenCalledTimes(1)
+
+            expect(consoleLogMock).toHaveBeenCalledWith(
+                chalk.red(
+                    '\nLooks like component already exists, please try deleting its directory and try again.'
+                )
+            )
+        })
+    })
+
+    describe('With types and styles excluded', () => {
+        beforeAll(() => {
+            fs.rmSync(targetDirectory, { recursive: true, force: true })
+        })
+
+        beforeEach(() => {
+            consoleLogMock.mockClear()
+        })
+
+        afterAll(() => {
+            fs.rmSync(targetDirectory, { recursive: true, force: true })
+        })
+
+        it('should log successful messages', async () => {
+            await createComponent(componentName, sandboxDirName, true, true)
+
+            expect(consoleLogMock).toHaveBeenCalledTimes(2)
+
+            expect(consoleLogMock).toHaveBeenCalledWith(
+                chalk.green('\nComponent has been successfully created!')
+            )
+
+            expect(consoleLogMock).toHaveBeenCalledWith(
+                chalk.cyan('\nHappy coding 🎉')
+            )
+        })
+
+        it('should create files correctly with correct content', () => {
+            expect(
+                fileContainsContent(indexFilePath, mockIndexTemplate)
+            ).toEqual(true)
+
+            expect(
+                fileContainsContent(
+                    componentFilePath,
+                    mockComponentTemplateBasic
+                )
+            ).toEqual(true)
+
+            expect(fs.existsSync(stylesFilePath)).toEqual(false)
+
+            expect(fs.existsSync(typesPath)).toEqual(false)
+        })
+
+        it('should log error message when trying to create component in existing directory', async () => {
+            await createComponent(componentName, sandboxDirName, false, false)
+
+            expect(consoleLogMock).toHaveBeenCalledTimes(1)
+
+            expect(consoleLogMock).toHaveBeenCalledWith(
+                chalk.red(
+                    '\nLooks like component already exists, please try deleting its directory and try again.'
+                )
+            )
+        })
+    })
+})
---
+++ src/utils/components/mocks.ts
@@ -0,0 +1,39 @@
+import * as path from 'path'
+import * as fs from 'fs'
+
+export const componentName = 'TestComponent'
+
+export const indexFilename = 'index.ts'
+
+export const componentFileName = 'TestComponent.tsx'
+
+export const stylesFileName = 'TestComponentStyles.ts'
+
+export const typesFileName = 'types.ts'
+
+export const sandboxDirName = 'sandbox'
+
+export const currentDirectory = process.cwd()
+
+export const targetDirectory = path.join(currentDirectory, sandboxDirName)
+
+export const componentDirectory = path.join(targetDirectory, componentName)
+
+export const indexFilePath = path.join(componentDirectory, indexFilename)
+
+export const componentFilePath = path.join(
+    componentDirectory,
+    componentFileName
+)
+
+export const stylesFilePath = path.join(componentDirectory, stylesFileName)
+
+export const typesPath = path.join(componentDirectory, typesFileName)
+
+export const fileContainsContent = (
+    filePath: string,
+    expectedContent: string
+) => {
+    const fileContent = fs.readFileSync(filePath, 'utf8')
+    return fileContent.trim() === expectedContent.trim()
+}
