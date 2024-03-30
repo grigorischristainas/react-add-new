b8b01cd000bff5950dae2a9b4ea33a68e57e1dee
Grigoris Christainas <grigorisxristainas@gmail.com>
2024-03-30 11:13:58 +0200

    Updated createInteractive, replaced promise chaining with async/await

--- src/utils/components/createInteractive.ts
+++ src/utils/components/createInteractive.ts
@@ -9,51 +9,45 @@ type Answer = {
     withTypes: boolean
 }
 
-const createInteractive = () => {
+const createInteractive = async () => {
     inquirer.registerPrompt('fuzzypath', require('inquirer-fuzzy-path'))
+    try {
+        const { name, path, withStyles, withTypes }: Answer =
+            await inquirer.prompt([
+                {
+                    name: 'name',
+                    message: 'What is your component name?',
+                },
+                {
+                    type: 'fuzzypath',
+                    name: 'path',
+                    excludePath: (nodePath: string) =>
+                        nodePath.startsWith('node_modules') ||
+                        nodePath.startsWith('dist'),
+                    excludeFilter: (nodePath: string) =>
+                        nodePath.startsWith('.'),
+                    message: 'Select a target directory for your component:',
+                    suggestOnly: true,
+                    depthLimit: 5,
+                },
+                {
+                    name: 'withStyles',
+                    type: 'confirm',
+                    message: 'Add styles?',
+                },
+                {
+                    name: 'withTypes',
+                    type: 'confirm',
+                    message: 'Add types?',
+                },
+            ])
 
-    inquirer
-        .prompt([
-            {
-                name: 'name',
-                message: 'What is your component name?',
-            },
-            {
-                type: 'fuzzypath',
-                name: 'path',
-                excludePath: (nodePath: string) =>
-                    nodePath.startsWith('node_modules') ||
-                    nodePath.startsWith('dist'),
-                excludeFilter: (nodePath: string) => nodePath.startsWith('.'),
-                message: 'Select a target directory for your component:',
-                suggestOnly: true,
-                depthLimit: 5,
-            },
-            {
-                name: 'withStyles',
-                type: 'confirm',
-                message: 'Add styles?',
-            },
-            {
-                name: 'withTypes',
-                type: 'confirm',
-                message: 'Add types?',
-            },
-        ])
-        .then(({ name, path, withStyles, withTypes }: Answer) => {
-            createComponent(name, path, !withStyles, !withTypes)
-        })
-        .catch((error) => {
-            if (error.isTtyError) {
-                logError(
-                    'Prompt could not be rendered in the current environment.'
-                )
-            } else {
-                logError(
-                    'Something went wrong during component generation, please try again.'
-                )
-            }
-        })
+        await createComponent(name, path, !withStyles, !withTypes)
+    } catch (error) {
+        logError(
+            'Something went wrong during component generation, please try again.'
+        )
+    }
 }
 
 export default createInteractive
