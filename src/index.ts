7980a6ad24023e8c7206eff5f364d2150ea6bca0
Antonis Balasas <axaros@gmail.com>
2024-10-29 00:39:59 +0200

    Publicly expose validateName

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as createComponent } from './utils/components/createComponent'
 export { default as createContext } from './utils/context/createContext'
+export { default as validateName } from './utils/validators/validateName'
